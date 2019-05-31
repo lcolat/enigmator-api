239c96507b555d3ace4858103ab2901faffef9aa
Kalfaa <theo92320@hotmail.fr>
2019-05-31 15:02:13 +0200

    Changement de certain droit + Ajout d'une route pour les requetes d'amis

--- common/models/friends.js
+++ common/models/friends.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function(Friends) {
-  // Friends.disableRemoteMethodByName('upsert');
+module.exports = function(Friends)  {
+  Friends.disableRemoteMethodByName('insert');
+  Friends.prototype.SendFriendRequest = function(id, options, callback) {
+    // TODO
+    const token = options && options.accessToken;
+    const userId = token && token.userId;
+    const user = userId ? 'user#' + userId : '<anonymous>';
+    console.log(user);
+    var app = Friends.app;
+    var Userenigmator = app.models.UserEnigmator;
+    var friend = Userenigmator.findById(id, function(err, friend) {
+      console.log(friend);
+      if (friend !== undefined) {
+        var friendRequest = {
+          state: 'request',
+          ID_FROM: userId,
+          ID_TO: id
+        };
+        Friends.create(friendRequest);
+      }
+      callback(err);
+    });
+    console.log(friend);
+  };
 };
--- common/models/user-enigmator.js
+++ common/models/user-enigmator.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function(Userenigmator) {
-  // Friends.disableRemoteMethodByName('upsert');
+  Userenigmator.disableRemoteMethodByName('upsert');
+  Userenigmator.disableRemoteMethodByName('prototype.__destroyById__accessTokens'); // DELETE
+  Userenigmator.disableRemoteMethodByName('prototype.__updateById__accessTokens'); // PUT
 };
 

4008a1694e062f43ba51604154e029474c671e6d
Kalfaa <theo92320@hotmail.fr>
2019-05-31 16:07:24 +0200

    Add error message for AddFriend

--- common/models/friends.js
+++ common/models/friends.js
@@ -3,25 +3,37 @@
 module.exports = function(Friends)  {
   Friends.disableRemoteMethodByName('create');
   Friends.prototype.SendFriendRequest = function(id, options, callback) {
-    // TODO
+    var app = Friends.app;
+    var Userenigmator = app.models.UserEnigmator;
     const token = options && options.accessToken;
     const userId = token && token.userId;
     const user = userId ? 'user#' + userId : '<anonymous>';
-    console.log(user);
-    var app = Friends.app;
-    var Userenigmator = app.models.UserEnigmator;
-    var friend = Userenigmator.findById(id, function(err, friend) {
+    var result = {};
+
+    Userenigmator.findById(id, function(err, friend) {
       console.log(friend);
       if (friend !== undefined) {
         var friendRequest = {
           state: 'request',
           ID_FROM: userId,
-          ID_TO: id
+          ID_TO: id,
         };
-        Friends.create(friendRequest);
+        Friends.find({where: friendRequest}, function(err, models) {
+          console.log(models);
+          if (models.length === 0) {
+            Friends.create(friendRequest);
+            result['message'] = "Requete d'amis créer";
+          } else {
+            console.log('alreadyhere');
+            result = {
+              statusCode: 422,
+              message: "La requète d'amis a deja été envoyé",
+            };
+          }
+          callback(result);
+        });
       }
-      callback(err);
+      // callback(result);
     });
-    console.log(friend);
   };
 };

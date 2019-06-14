85cf4f420e0283ae17dd04b531bf4b14acae7ada
Kalfa <theo92320@hotmail.fr>
2019-06-14 18:48:17 +0200

    AnswerEnigme

--- common/models/enigme.js
+++ common/models/enigme.js
@@ -2,4 +2,41 @@
 
 module.exports = function(Enigme) {
   // Friends.disableRemoteMethodByName('upsert');
+
+  Enigme.prototype.AnswerEnigme = function(id, answer, options ,callback) {
+    // TODO
+    var app = Enigme.app
+    var UserEnigmator = app.models.UserEnigmator;
+    var History = app.models.History;
+    const token = options && options.accessToken;
+    const userId = token && token.userId;
+    const user = userId ? 'user#' + userId : '<anonymous>';
+    var result ;
+    Enigme.findById(id,{},function(err,enigme){
+      console.log(enigme);
+      console.log(answer);
+      //TODO CHANGE TYPE ANWSER
+      if(answer === enigme['anwser'] ){
+         result = {
+          message : 'bonne réponse ! ',
+        };
+         var userHistory = {
+            userEnigmatorId : user,
+            enigmeId : id ,
+            type : 'success',
+           //date :
+         };
+         History.create(userHistory);
+        callback(null,result);
+      }else{
+        result = {
+          message : 'mauvaise réponse ! ',
+        };
+        callback(null,result);
+      }
+
+    });
+
+  };
+
 };

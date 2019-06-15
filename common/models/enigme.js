e0d3a9d95d682e0e70901e587217eb19a665e5a5
Kalfaa <theo92320@hotmail.fr>
2019-06-15 20:41:11 +0200

    Start upload in createEnigme

--- common/models/enigme.js
+++ common/models/enigme.js
@@ -5,49 +5,56 @@ module.exports = function(Enigme) {
 
   Enigme.prototype.AnswerEnigme = function(id, answer, options, callback) {
     // TODO
-    var now = new Date() ;
+    var now = new Date();
     var app = Enigme.app;
     var UserEnigmator = app.models.UserEnigmator;
     var History = app.models.History;
     const token = options && options.accessToken;
     const userId = token && token.userId;
     const user = userId ? 'user#' + userId : '<anonymous>';
-    var result ;
-    Enigme.findById(id,{},function(err,enigme){
-      if(answer === enigme['answer'] ){
-         result = {
-          message : 'bonne réponse ! ',
+    var result;
+    Enigme.findById(id, {}, function(err, enigme) {
+      if (answer === enigme['answer']) {
+        result = {
+          message: 'bonne réponse ! ',
         };
-         var userHistory = {
-            userEnigmatorId : user,
-            enigmeId : id ,
-            type : 'success',
-            date : now.toJSON()
-         };
-         History.create(userHistory);
-         //TODO ADD THE SCORE TO USER RANK
-        UserEnigmator.findById(userId,{},function(err,userData){
+        var userHistory = {
+          userEnigmatorId: user,
+          enigmeId: id,
+          type: 'success',
+          date: now.toJSON(),
+        };
+        History.create(userHistory);
+         // TODO ADD THE SCORE TO USER RANK
+        UserEnigmator.findById(userId, {}, function(err, userData) {
           userData['rank'] += enigme['scoreReward'];
-          UserEnigmator.upsert(userData,function(err,obj){
-
-            callback(null,result);
+          UserEnigmator.upsert(userData, function(err, obj) {
+            callback(null, result);
           });
         });
-        UserEnigmator.upsert()
-         callback(null,result);
-      }else{
+        UserEnigmator.upsert();
+        callback(null, result);
+      } else {
         result = {
-          message : 'mauvaise réponse ! ',
+          message: 'mauvaise réponse ! ',
         };
-        callback(null,result);
+        callback(null, result);
       }
-
     });
-
   };
-
-  Enigme.CreateEnigme = function(data, file, callback) {
-    // TODO
-    callback(null);
+  Enigme.CreateEnigme = function(req, res, data, callback) {
+    console.log('coucou');
+    var app = Enigme.app;
+    var Container = app.models.Media;
+    // console.log(res);
+    // console.log(req);
+    Container.upload(req, res, {container: '/enigme'}, function(err, data) {
+      if  (err) {
+        callback(err);
+        console.log(err);
+      }      else  {
+        callback(err, data);
+      }
+    });
   };
 };
